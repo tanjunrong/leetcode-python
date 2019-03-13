3158cb1e5dade1054fd1199145cdf8fed4566090
tanjunrong <739574029@qq.com>
2019-03-13 10:59:29 +0800

    //Q1

---
+++ src/solutions/Q3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_number = 0
+        number = 0
+        sub = ""
+        for i in s:
+            if i in sub:
+                max_number = max(number, max_number)
+                number -= sub.index(i)
+                sub = sub[(sub.index(i) + 1):]
+            else:
+                number += 1
+            sub += i
+        return max(max_number, number)
+
+
